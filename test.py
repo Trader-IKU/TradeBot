9b1d5366bde9f2a3d6c48df871eaa3d5904e7100
IKU-Trader <docs910@gmail.com>
2024-01-08 22:19:01 +0900

    update

--- test.py
+++ test.py
@@ -12,23 +12,21 @@ from backtest_ga import server_time_str_2_datetime
 
 
 def plot(data: dict):
-    fig, axes = makeFig(2, 1, (20, 15))
-    chart = CandleChart(fig, axes[0])
+    fig, ax = makeFig(1, 1, (20, 15))
+    chart = CandleChart(fig, ax)
     chart.drawCandle(data[Columns.TIME], data[Columns.OPEN], data[Columns.HIGH], data[Columns.LOW], data[Columns.CLOSE])
     chart.drawLine(data[Columns.TIME], data['MA60'], color='blue')
     chart.drawLine(data[Columns.TIME], data[Indicators.ATR_U], color='green')
     chart.drawLine(data[Columns.TIME], data[Indicators.ATR_L], color='red')
-    chart2 = CandleChart(fig, axes[1])
-    #chart2.drawCandle(data[Columns.TIME], data[Columns.OPEN], data[Columns.HIGH], data[Columns.LOW], data[Columns.CLOSE])
-    chart2.drawLine(data[Columns.TIME], data['VOLATILITY'], color='blue')
-    #chart2.drawLine(data[Columns.TIME], data[Indicators.SUPERTREND_U], color='green')
-    #chart2.drawLine(data[Columns.TIME], data[Indicators.SUPERTREND_L], color='red')
+    ax2 = ax.twinx()
+    chart2 = CandleChart(fig, ax2)
+    chart2.drawLine(data[Columns.TIME], data['VOLATILITY'], color='orange', linewidth=3.0)
     plt.show()
     pass
 
 def test():
     symbol = 'USDJPY'
-    timeframe = 'M5'
+    timeframe = 'M15'
     params = {'MA':{'window': 60}, 'VOLATILITY':{'window': 50}, 'ATR': {'window': 20, 'multiply':2.0}}
     
     data = load_data(symbol, timeframe, [2024], [1])
