93b1b479950a94cfe4a7c57b54e8927b4925d3fb
Trader-IKU <trader.iku19@gmail.com>
2024-02-11 22:21:05 +0900

    fix bug

--- analyze.py
+++ analyze.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../Libraries/trade')
 
 import shutil
-from backtest_ga import load_data
+from backtest import DataLoader, TradeBotSim, PositionInfoSim, indicators
 from mt5_trade import Mt5TradeSim, Columns, JST, UTC
 from datetime import datetime, timedelta
 from candle_chart import CandleChart, BandPlot, makeFig, gridFig
@@ -80,35 +80,115 @@ def plot_charts(symbol, timeframe, data: dict, days=7, is_first_monday=True):
             count += 1
         t += timedelta(days=days)        
         
-  
-def indicators(data: dict, param):
-    atr_window = param['atr_window']
-    atr_multiply = param['atr_multiply']
-    di_window = param['di_window']
-    adx_window = param['adx_window']
-    polarity_window = param['polarity_window']
-    
-    MA(data, Columns.CLOSE, 5)
-    ATR(data, atr_window, atr_window * 2, atr_multiply)
-    ADX(data, di_window, adx_window, adx_window * 2)
-    POLARITY(data, polarity_window)
-    TREND_ADX_DI(data, 20)
-    SUPERTREND(data)
 
+def pickup_trade(trades, tbegin, tend):
+    out = []
+    for trade in trades:
+        if trade.entry_time >= tbegin and trade.entry_time <= tend:
+            out.append(trade)
+    return out
+
+def plot(data: dict, trades, save, chart_num=0):
+    fig, axes = gridFig([5, 1], (20, 10))
+    chart1 = CandleChart(fig, axes[0])
+    chart1.drawCandle(data[Columns.TIME], data[Columns.OPEN], data[Columns.HIGH], data[Columns.LOW], data[Columns.CLOSE])
+    #name = 'MA' + str(params['MA']['window'])
+    #chart.drawLine(data[Columns.TIME], data[name], color='blue')
+    chart1.drawLine(data[Columns.TIME], data[Indicators.SUPERTREND_UPPER], color='red', linewidth=2.0)
+    chart1.drawLine(data[Columns.TIME], data[Indicators.SUPERTREND_LOWER], color='green', linewidth=2.0)
+    
+    chart2 = CandleChart(fig, axes[1])
+    chart2.drawLine(data[Columns.TIME], data[Indicators.SUPERTREND])
+    
+    for i, trade in enumerate(trades):
+        trade.desc()
+        if trade.signal() == Signal.LONG:
+            marker = '^'
+            color = 'green'
+        else:
+            marker = 'v'
+            color = 'red'
+        chart1.drawMarker(trade.entry_time, trade.entry_price, marker, color, markersize=5.0, overlay=i)
+        
+        if trade.losscutted:
+            marker = 'x'
+        elif trade.trailing_stopped:
+            marker = '*'
+        elif trade.time_upped:
+            marker = 's'
+        else:
+            marker = 'o'
+        if trade.profit is not None:
+            if trade.profit < 0:
+                color = 'gray'
+        chart1.drawMarker(trade.exit_time, trade.exit_price, marker, color, overlay=i)            
+    if save:
+        plt.savefig('./charts/fig' + str(chart_num) + '.png')
+    else:
+        plt.show()
+    
+                
+def plot_days(data, trades, days=7, is_start_monday=True):
+    def next_monday(t: datetime):
+        t1 = t
+        while True:
+            if t1.weekday() == 0:
+                return t1
+            t1 += timedelta(days=1)        
+            return t1
+    time = data[Columns.TIME]
+    if is_start_monday:
+        t = next_monday(time[0])
+        t = datetime(t.year, t.month, t.day, tzinfo=JST)
+    else:
+        t = time[0]
+
+    tend = time[-1]
+    tend = datetime(tend.year, tend.month, tend.day, tzinfo=JST) + timedelta(days=1)
+    count = 1
+    while t < tend:
+        t1 = t + timedelta(days=days)
+        try:
+            n, d = Utils.sliceBetween(data, time, t, t1)
+            if n < 40:
+                t += timedelta(days=days)
+                continue
+        except:
+            t += timedelta(days=days)
+            continue
+        trds = pickup_trade(trades, t, t1)
+        plot(d, trds, True, count)
+        count += 1
+        t += timedelta(days=days)        
+        
 
 def simulate(symbol, timeframe):
-    year = 2023
-    data = load_data(symbol, timeframe, [year], range(1, 13))
-    p = {'atr_window': 50, 'atr_multiply': 3.0, 'di_window': 25, 'adx_window': 25, 'polarity_window': 50}
-    indicators(data, p)
-    plot_charts(symbol, timeframe, data, days=7, is_first_monday=True)
+    loader = DataLoader()
+    n = loader.load_data(symbol, timeframe, range(2020, 2024), range(1, 13))
+    technical_param = {'atr_window': 20, 'atr_multiply': 0.8}
+    data = loader.data()
+    indicators(data, technical_param)
+    trade_param =  {'sl':350, 'target_profit': 400, 'trailing_stop': 200, 'volume': 0.1, 'position_max': 5, 'timelimit': 0}
+    sim = TradeBotSim(symbol, timeframe, trade_param)
+    sim.run(data, 150)
+    while True:
+        r = sim.update()
+        if r == False:
+            break
+    trades = sim.positions
+    (df, profit, num, profit_max, profit_min, win_rate) = PositionInfoSim.summary(trades)
+    df['entry_time'] = [str(t) for t in df['entry_time']]
+    df['exit_time'] = [str(t) for t in df['exit_time']]
+    df.to_excel('./result/trade_summary_' + symbol + '_' + timeframe + '.xlsx')
+    print(symbol, timeframe, 'profit', profit, 'drawdown', profit_min, 'num', num, )
+    plot_days(data, trades)
     pass        
 
 def test():
     shutil.rmtree('./charts/')
     os.makedirs('./charts', exist_ok=True)
     symbol = 'NIKKEI'
-    timeframe = 'M15'
+    timeframe = 'M30'
     simulate(symbol, timeframe)
     
 if __name__ == '__main__':
--- backtest.py
+++ backtest.py
@@ -75,9 +75,9 @@ class DataLoader:
 def indicators(data: dict, param: dict):
     atr_window = param['atr_window']
     atr_multiply = param['atr_multiply']
-    di_window = param['di_window']
-    adx_window = param['adx_window']
-    polarity_window = param['polarity_window']
+    #di_window = param['di_window']
+    #adx_window = param['adx_window']
+    #polarity_window = param['polarity_window']
     
     #MA(data, Columns.CLOSE, 5)
     ATR(data, atr_window, atr_window * 2, atr_multiply)
@@ -178,7 +178,7 @@ class PositionInfoSim(PositionInfo):
         return count
     
     @staticmethod 
-    def summary(trades, save=False, plot=False):
+    def summary(trades):
         profit = 0 
         num = 0
         profit_max = None
@@ -203,15 +203,14 @@ class PositionInfoSim(PositionInfo):
                 if pos.profit < profit_min:
                     profit_min = pos.profit
             num += 1
-            result.append([pos.signal, pos.entry_index, str(pos.entry_time), pos.entry_price, pos.exit_index, pos.exit_time, pos.exit_price, pos.profit])
+            result.append([pos.signal(), pos.entry_index, str(pos.entry_time), pos.entry_price, pos.exit_index, pos.exit_time, pos.exit_price, pos.profit, pos.losscutted, pos.trailing_stopped ])
         if num == 0:
             return (0, 0, 0, 0, 0)
         else:
             win_rate =  float(win) / float(num)
-            if save:
-                columns = ['Long/Short', 'entry_index', 'entry_time', 'entry_price', 'exit_index', 'exit_time', 'exit_price', 'profit']
-                df = pd.DataFrame(data=result, columns=columns)
-            return (profit, num, profit_max, profit_min, win_rate)
+            columns = ['Long/Short', 'entry_index', 'entry_time', 'entry_price', 'exit_index', 'exit_time', 'exit_price', 'profit', 'losscutted', 'trailing_stopped']
+            df = pd.DataFrame(data=result, columns=columns)
+            return (df, profit, num, profit_max, profit_min, win_rate)
             
 class TradeBotSim:
     def __init__(self, symbol: str, timeframe: str, trade_param: dict):
@@ -277,10 +276,10 @@ class TradeBotSim:
         price = cl[index]
         target = self.trade_param['target_profit']
         sl = self.trade_param['sl']
-        if sl == 0:
-            atr = data[Indicators.ATR]
-            sl = atr[index] * 2.0
-            #print('Set Stoploss: ', sl)
+        #if sl == 0:
+        #    atr = data[Indicators.ATR]
+        #    sl = atr[index] * 2.0
+        #    #print('Set Stoploss: ', sl)
         volume = self.trade_param['volume']
         if signal == Signal.LONG:
             typ = mt5.ORDER_TYPE_BUY
@@ -297,97 +296,9 @@ class TradeBotSim:
             position.close(index, time, price)
         
         
-def pickup_trade(trades, tbegin, tend):
-    out = []
-    for trade in trades:
-        if trade.entry_time >= tbegin and trade.entry_time <= tend:
-            out.append(trade)
-    return out
 
-def plot(data: dict, trades):
-    fig, axes = gridFig([5, 1], (20, 10))
-    chart1 = CandleChart(fig, axes[0])
-    chart1.drawCandle(data[Columns.TIME], data[Columns.OPEN], data[Columns.HIGH], data[Columns.LOW], data[Columns.CLOSE])
-    #name = 'MA' + str(params['MA']['window'])
-    #chart.drawLine(data[Columns.TIME], data[name], color='blue')
-    chart1.drawLine(data[Columns.TIME], data[Indicators.SUPERTREND_UPPER], color='red', linewidth=2.0)
-    chart1.drawLine(data[Columns.TIME], data[Indicators.SUPERTREND_LOWER], color='green', linewidth=2.0)
-    
-    chart2 = CandleChart(fig, axes[1])
-    chart2.drawLine(data[Columns.TIME], data[Indicators.SUPERTREND])
-    
-    for i, trade in enumerate(trades):
-        trade.desc()
-        if trade.signal() == Signal.LONG:
-            marker = '^'
-            color = 'green'
-        else:
-            marker = 'v'
-            color = 'red'
-        chart1.drawMarker(trade.entry_time, trade.entry_price, marker, color, overlay=i)
-        
-        if trade.losscutted:
-            marker = 'x'
-        elif trade.trailing_stopped:
-            marker = '*'
-        elif trade.time_upped:
-            marker = 's'
-        else:
-            marker = 'o'
-        if trade.profit is not None:
-            if trade.profit < 0:
-                color = 'gray'
-        chart1.drawMarker(trade.exit_time, trade.exit_price, marker, color, overlay=i)            
-    plt.show()
-                
-def plot_days(data, trades, days=7, is_start_monday=True):
-    def next_monday(t: datetime):
-        t1 = t
-        while True:
-            if t1.weekday() == 0:
-                return t1
-            t1 += timedelta(days=1)        
-            return t1
-    time = data[Columns.TIME]
-    if is_start_monday:
-        t = next_monday(time[0])
-        t = datetime(t.year, t.month, t.day, tzinfo=JST)
-    else:
-        t = time[0]
 
-    tend = time[-1]
-    tend = datetime(tend.year, tend.month, tend.day, tzinfo=JST) + timedelta(days=1)
-    while t < tend:
-        t1 = t + timedelta(days=days)
-        try:
-            n, d = Utils.sliceBetween(data, time, t, t1)
-            if n < 40:
-                t += timedelta(days=days)
-                continue
-        except:
-            t += timedelta(days=days)
-            continue
-        trds = pickup_trade(trades, t, t1)
-        plot(d, trds)
-        t += timedelta(days=days)        
 
-def backtest(symbol, timeframe):
-    loader = DataLoader()
-    n = loader.load_data(symbol, timeframe, [2024], [1])
-    technical_param = {'atr_window': 50, 'atr_multiply': 3.0, 'di_window': 25, 'adx_window': 25, 'polarity_window': 50}
-    data = loader.data()
-    indicators(data, technical_param)
-    trade_param =  {'sl':100, 'trailing_stop': 150, 'volume': 0.1, 'position_max': 5, 'timelimit': 0}
-    sim = TradeBotSim(symbol, timeframe, trade_param)
-    sim.run(data, 150)
-    while True:
-        r = sim.update()
-        if r == False:
-            break
-    trades = sim.positions
-    (profit, num, profit_max, profit_min, win_rate) = PositionInfoSim.summary(trades)
-    print(symbol, timeframe, 'profit', profit, 'drawdown', profit_min, 'num', num, )
-    plot_days(data, trades)
     
 def optimize_trade(symbol, timeframe, gene_space, years, months, number, repeat=100, save_every=False):
     loader = DataLoader()
@@ -427,7 +338,7 @@ def optimize_trade(symbol, timeframe, gene_space, years, months, number, repeat=
             if r == False:
                 break
         trades = sim.positions
-        (profit, num, profit_max, profit_min, win_rate) = PositionInfoSim.summary(trades)
+        (_, profit, num, profit_max, profit_min, win_rate) = PositionInfoSim.summary(trades)
         result.append([symbol, timeframe, years[0], years[-1], atr_window, atr_multiply, sl, target_profit, trailing_stop, profit, num, profit_min, profit + profit_min, win_rate])
         count += 1
         print('#' + str(count), symbol, timeframe, 'profit', profit, 'drawdown', profit_min, 'num', num, 'win_rate', win_rate )    
@@ -466,9 +377,9 @@ def create_gene_space(symbol, timeframe):
     else:
         raise Exception('Bad symbol')
 
-    d = [0.0, 0.0, 0.0] + list(np.arange(r[1], r[2], r[3]))
-    sl = [GeneticCode.GeneList, d] 
-    trailing_stop = sl
+    d = [0.0] + list(np.arange(r[1], r[2], r[3]))
+    sl = r
+    trailing_stop =  [GeneticCode.GeneList, d] 
     target = r
     
     technical_space = [
